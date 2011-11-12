83269958c935b163825a95cbbe6c67f176a8d6c7
Ash McKenzie <ash.mckenzie@playup.com>
2011-11-13 01:38:22 +1100

    more dynamic

--- public/javascripts/socket.js
+++ public/javascripts/socket.js
@@ -2,14 +2,12 @@ $(document).ready(function() {
   var socket = io.connect('http://localhost');
 
   socket.on('message', function(services) {
+    var s = $('#services');
+    s.html('');
     for (var i=0 ; i <= (services.length - 1); i++) {
       var name = services[i].name;
       var status = services[i].status;
-      var service = $('#' + name);
-      var service_status = service.find('.service-status');
-      var last_updated = service.find('.last-updated-time');
-      service_status.attr('data-status', status);
-      last_updated.html(' ' + Date());
+      s.append('<div id="' + name + '" class="service"> <div class="service-status" data-status="' + status + '"></div> <p>' + name + '</p> <p class="last-updated">last updated<span class="last-updated-time"> ' + Date() + '</span></p> </div> ');
     }
   });
 });
